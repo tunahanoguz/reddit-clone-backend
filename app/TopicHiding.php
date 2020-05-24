9435bfe8692a567b0efc6478b26fbc52c6c7fe24
tunahanoguz <tunahanog@gmail.com>
2020-05-24 13:36:59 +0300

    TopicHiding model was created.

--- app/TopicHiding.php
+++ app/TopicHiding.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class TopicHiding extends Model
 {
-    //
+    protected $fillable = ['user_id', 'topic_id'];
+    public $timestamps = false;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function topic() {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->created_at = $model->freshTimestamp();
+        });
+    }
 }
