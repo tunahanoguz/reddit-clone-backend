fd41e7488781d984925039d6f5e49de892bc26b4
tunahanoguz <tunahanog@gmail.com>
2020-05-25 20:39:55 +0300

    Addes status code in JwtMiddleware.

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -23,11 +23,11 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is invalid.']);
+                return response()->json(['status' => 'Token is invalid.'], 401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is expired.']);
+                return response()->json(['status' => 'Token is expired.'], 401);
             }else{
-                return response()->json(['status' => 'Authorization token was not found']);
+                return response()->json(['status' => 'Authorization token was not found.'], 401);
             }
         }
         return $next($request);
